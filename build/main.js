9f31fe66bb73e93bde5a222219d467a1c2bdbe95
Dawid <dawid.marchewczyk@gmail.com>
2024-03-31 06:58:14 +0200

    Starter files, some basic styles

---
+++ build/main.js
@@ -0,0 +1,21 @@
+// const url =
+//   "https://car-data.p.rapidapi.com/cars?limit=10&page=0&make=MERCEDES&model=CLA";
+// const options = {
+//   method: "GET",
+//   headers: {
+//     "X-RapidAPI-Key": "d09c0e749cmsh3aaf1f8156902d7p165ca7jsn042ccef52cc6",
+//     "X-RapidAPI-Host": "car-data.p.rapidapi.com",
+//   },
+// };
+
+// const getData = async () => {
+//   try {
+//     const response = await fetch(url, options);
+//     const result = await response.json();
+//     console.log(result);
+//   } catch (error) {
+//     console.error(error);
+//   }
+// };
+
+// getData();
